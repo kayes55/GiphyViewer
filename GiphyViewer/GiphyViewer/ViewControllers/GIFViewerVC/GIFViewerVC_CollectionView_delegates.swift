51213ed505d1567c2d5a0638e5c47247d4cc379a
Kayes <kayes.shamil@gmail.com>
2019-02-04 13:09:22 +0600

    Added GIF Viewer CollectionView

---
+++ GiphyViewer/GiphyViewer/ViewControllers/GIFViewerVC/GIFViewerVC_CollectionView_delegates.swift
@@ -0,0 +1,23 @@
+//
+//  GIFViewerVC_CollectionView_delegates.swift
+//  GiphyViewer
+//
+//  Created by Imrul Kayes on 2/4/19.
+//  Copyright © 2019 Imrul Kayes. All rights reserved.
+//
+
+import UIKit
+
+extension GIFViewerVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "gifCell", for: indexPath) as! GifViwerCell
+        
+        return cell
+    }
+    
+    
+}
---
+++ GiphyViewer/GiphyViewer/ViewControllers/GIFViewerVC/Views/GifViwerCell.swift
@@ -0,0 +1,15 @@
+//
+//  GifViwerCell.swift
+//  GiphyViewer
+//
+//  Created by Imrul Kayes on 2/4/19.
+//  Copyright © 2019 Imrul Kayes. All rights reserved.
+//
+
+import UIKit
+
+class GifViwerCell: UICollectionViewCell {
+    @IBOutlet weak var gifView: UIImageView!
+    @IBOutlet weak var gifName: UILabel!
+    
+}
